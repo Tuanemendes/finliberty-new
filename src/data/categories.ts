5ff57e577887e60e1ac2ee6f49cc2f5ef39ed0de
Tuanemendes <tuane.belmiro@gmail.com>
2023-07-17 23:26:54 -0300

    criação do data

---
+++ src/data/categories.ts
@@ -0,0 +1,9 @@
+import { Category } from "../types/Category";
+
+export const categories: Category = {
+
+    food:{title:'Alimentação',color:'yellow',expense:true},
+    rent:{title:'Aluguel',color:'blue',expense:true},
+    salary:{title:'Salário',color:'green',expense:false},
+}
+    
---
+++ src/data/itens.ts
@@ -0,0 +1,28 @@
+import { Item } from "../types/Item";
+
+export const itens: Item[] = [
+    {
+        date:new Date(2021, 0, 1),
+        category: 'Salary',
+        title: 'Salario TMB',
+        value: 2000.50
+    },
+    {
+        date:new Date(2021, 0, 1),
+        category: 'Food',
+        title: 'Saraivas food',
+        value: 2000.50
+    },
+    {
+        date:new Date(2021, 0, 1),
+        category: 'rent',
+        title: 'Aluguel',
+        value: 2000.50
+    },
+    {
+        date:new Date(2021, 0, 1),
+        category: 'Supermarket',
+        title: 'Compra de mês',
+        value: 2000.50
+    },
+]
