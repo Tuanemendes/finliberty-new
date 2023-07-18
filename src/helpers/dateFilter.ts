b2fcc1a88acff5bccfc388df6b5b1510e3f4f809
Tuanemendes <tuane.belmiro@gmail.com>
2023-07-17 23:27:56 -0300

    configuração e implementação do layout

---
+++ src/helpers/dateFilter.ts
@@ -0,0 +1,31 @@
+//lista de funções basedo em data  fazer um filtro de data formatar a data 
+
+import { Item } from "@/types/Item";
+
+export const getCurrentMonth = () => {
+    //pega a data atual
+    let now = new Date();
+    //retorna o mês atual +1 pq o mes começa em 0
+    return `${now.getFullYear()}-${now.getMonth()+1}`;
+}
+
+
+export const filterListByMonth = (list: Item[], date: string): Item[] => {
+    
+    //gerar uma lista com o mes atual
+    let newList: Item[] = [];
+   
+    
+    let [year, month] = date.split('-');
+
+    for(let i in list){
+        if(
+            list[i].date.getFullYear() === parseInt(year)&&
+            list[i].date.getMonth()+1 === parseInt(month)
+        ){
+            newList.push(list[i]);
+        }
+    }
+
+    return newList ;
+}
